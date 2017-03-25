9e5330aed8e6e63616554d6604374e7d54d40353
Hsuan Lin <Hsuan Lin>
2017-03-25 23:11:07 +0800

    reformat code

--- mrf_cnn/symbol.py
+++ mrf_cnn/symbol.py
@@ -1,36 +1,46 @@
 import mxnet as mx
 
+
 def symbol(num_res):
     data = mx.sym.Variable('data')
     weight_var = {}
     bias_var = {}
     convs = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1']
     for k in convs:
-        weight_var[k] = mx.sym.Variable(k+'_weight')
+        weight_var[k] = mx.sym.Variable(k + '_weight')
     for k in convs:
-        bias_var[k] = mx.sym.Variable(k+'_bias')
+        bias_var[k] = mx.sym.Variable(k + '_bias')
     out = vgg_symbol(data, weight_var, bias_var)
-    for i in range(num_res-1):
-        data = mx.sym.Pooling(data=data, kernel=(2,2), stride=(2,2), pad=(0,0), pool_type='avg')
+    for i in range(num_res - 1):
+        data = mx.sym.Pooling(data=data, kernel=(2, 2), stride=(2, 2), pad=(0, 0), pool_type='avg')
         out = mx.sym.Group([out, vgg_symbol(data, weight_var, bias_var)])
     return out
 
+
 def vgg_symbol(data, weight, bias):
     def conv(name, data, num_filter, pad, kernel, stride, no_bias, workspace):
-        return mx.sym.Convolution(name=name, data=data, weight=weight[name], bias=bias[name], num_filter=num_filter, pad=pad, kernel=kernel, stride=stride, no_bias=no_bias, workspace=workspace)
-    conv1_1 = conv(name='conv1_1', data=data , num_filter=64, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False, workspace=1024)
-    relu1_1 = mx.symbol.Activation(name='relu1_1', data=conv1_1 , act_type='relu')
-    conv1_2 = conv(name='conv1_2', data=relu1_1 , num_filter=64, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False, workspace=1024)
-    relu1_2 = mx.symbol.Activation(name='relu1_2', data=conv1_2 , act_type='relu')
-    pool1 = mx.symbol.Pooling(name='pool1', data=relu1_2 , pad=(0,0), kernel=(2,2), stride=(2,2), pool_type='avg')
-    conv2_1 = conv(name='conv2_1', data=pool1 , num_filter=128, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False, workspace=1024)
-    relu2_1 = mx.symbol.Activation(name='relu2_1', data=conv2_1 , act_type='relu')
-    conv2_2 = conv(name='conv2_2', data=relu2_1 , num_filter=128, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False, workspace=1024)
-    relu2_2 = mx.symbol.Activation(name='relu2_2', data=conv2_2 , act_type='relu')
-    pool2 = mx.symbol.Pooling(name='pool2', data=relu2_2 , pad=(0,0), kernel=(2,2), stride=(2,2), pool_type='avg')
-    conv3_1 = conv(name='conv3_1', data=pool2 , num_filter=256, pad=(1,1), kernel=(3,3), stride=(1,1), no_bias=False, workspace=1024)
-    relu3_1 = mx.symbol.Activation(name='relu3_1', data=conv3_1 , act_type='relu')
+        return mx.sym.Convolution(name=name, data=data, weight=weight[name], bias=bias[name], num_filter=num_filter,
+                                  pad=pad, kernel=kernel, stride=stride, no_bias=no_bias, workspace=workspace)
+
+    conv1_1 = conv(name='conv1_1', data=data, num_filter=64, pad=(1, 1), kernel=(3, 3), stride=(1, 1), no_bias=False,
+                   workspace=1024)
+    relu1_1 = mx.symbol.Activation(name='relu1_1', data=conv1_1, act_type='relu')
+    conv1_2 = conv(name='conv1_2', data=relu1_1, num_filter=64, pad=(1, 1), kernel=(3, 3), stride=(1, 1), no_bias=False,
+                   workspace=1024)
+    relu1_2 = mx.symbol.Activation(name='relu1_2', data=conv1_2, act_type='relu')
+    pool1 = mx.symbol.Pooling(name='pool1', data=relu1_2, pad=(0, 0), kernel=(2, 2), stride=(2, 2), pool_type='avg')
+    conv2_1 = conv(name='conv2_1', data=pool1, num_filter=128, pad=(1, 1), kernel=(3, 3), stride=(1, 1), no_bias=False,
+                   workspace=1024)
+    relu2_1 = mx.symbol.Activation(name='relu2_1', data=conv2_1, act_type='relu')
+    conv2_2 = conv(name='conv2_2', data=relu2_1, num_filter=128, pad=(1, 1), kernel=(3, 3), stride=(1, 1),
+                   no_bias=False, workspace=1024)
+    relu2_2 = mx.symbol.Activation(name='relu2_2', data=conv2_2, act_type='relu')
+    pool2 = mx.symbol.Pooling(name='pool2', data=relu2_2, pad=(0, 0), kernel=(2, 2), stride=(2, 2), pool_type='avg')
+    conv3_1 = conv(name='conv3_1', data=pool2, num_filter=256, pad=(1, 1), kernel=(3, 3), stride=(1, 1), no_bias=False,
+                   workspace=1024)
+    relu3_1 = mx.symbol.Activation(name='relu3_1', data=conv3_1, act_type='relu')
     return relu3_1
+
 
 class AssignPatch(mx.operator.NDArrayOp):
     def __init__(self):
@@ -47,7 +57,7 @@ class AssignPatch(mx.operator.NDArrayOp):
     def infer_shape(self, in_shape):
         nn_shape = in_shape[0]
         source_shape = in_shape[1]
-        return in_shape, [[1,source_shape[1], nn_shape[0]+source_shape[2]-1, nn_shape[1]+source_shape[3]-1]]
+        return in_shape, [[1, source_shape[1], nn_shape[0] + source_shape[2] - 1, nn_shape[1] + source_shape[3] - 1]]
 
     def forward(self, in_data, out_data):
         nn, source = in_data
@@ -66,7 +76,9 @@ class AssignPatch(mx.operator.NDArrayOp):
                     target_idx += target_dims[3]-source_dims[3];
                 }
             """)
-        self.fwd_kernel.push([nn, source], [target], (target.shape[2]-source.shape[2]+1, target.shape[3]-source.shape[3]+1, 1), (source.shape[1],1,1))
+        self.fwd_kernel.push([nn, source], [target],
+                             (target.shape[2] - source.shape[2] + 1, target.shape[3] - source.shape[3] + 1, 1),
+                             (source.shape[1], 1, 1))
 
 
 def assign_symbol():
